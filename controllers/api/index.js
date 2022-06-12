9468b4fec3f6430641d5eba24d63a7b2516a8404
Jessica Chen <jessicachen28@gmail.com>
2022-06-12 07:13:12 -0400

    Change Categories Route Path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -10,7 +10,7 @@ const commentRoutes = require("./comment-routes");
 // Prefix Grouped Endpoints with Specific Paths
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
-router.use("/category", categoryRoutes);
+router.use("/categories", categoryRoutes);
 router.use("/comments", commentRoutes);
 
 module.exports = router;
